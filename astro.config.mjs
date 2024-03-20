0f28d35d50e95fb430718d4718b49c113ecefdb5
Joseph Meza <joseph.meza@unmsm.edu.pe>
2024-03-20 06:50:22 -0500

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import netlify from '@astrojs/netlify'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 })
